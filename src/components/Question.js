4902a9c1f1f8f9e3f07534d5b8c8b33f496040ea
William Mosto <will.mosto@gmail.com>
2021-09-07 18:04:53 -0400

    commit tree 26dd7e97e6040742fc74f0a1984cab8020b46346

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      // console.log('This will run after 1 second!')
+      setTimeRemaining(timeRemaining - 1)
+    }, 1000);
+    return function (){
+      clearTimeout(time)
+    }
+  }, [timeRemaining]);
+
+  useEffect(() => {
+    setTimeRemaining(10)
+    setTimeout(() => onAnswered(false), 10000);
+  }, []);
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
